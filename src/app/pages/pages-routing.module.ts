502f7d872ecd342ec67109b7f1d3bfeafa60c276
xxGreyscale <salim.nassor.sn@gmail.com>
2021-10-26 10:58:52 +0300

    Feat: (...) Added error page and edited socials available

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
+import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: PagesComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
   }
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,15 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { PagesComponent } from './pages.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    ErrorPageComponent,
+    PagesComponent
 
   ],
   imports: [
